5cf5fc02295a5adb9ec80d32f535207932dd56c4
김여준 <kt65346337@gmail.com>
2025-01-29 11:39:13 +0900

    fix: 엔드포인트 수정

--- be-test/deploy-be/src/main/java/com/example/deploybe/controller/TestController.java
+++ be-test/deploy-be/src/main/java/com/example/deploybe/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class TestController {
-    @GetMapping
+    @GetMapping("/")
     public String hello() {
         return "Hello World";
     }
