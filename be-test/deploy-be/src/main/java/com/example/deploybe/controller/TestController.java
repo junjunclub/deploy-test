d921b87cb20020b152044e6fc96112fafc56d7ea
김여준 <kt65346337@gmail.com>
2025-01-29 12:16:35 +0900

    feat: 요청 URL 로그 추가

--- be-test/deploy-be/src/main/java/com/example/deploybe/controller/TestController.java
+++ be-test/deploy-be/src/main/java/com/example/deploybe/controller/TestController.java
@@ -1,19 +1,28 @@
 package com.example.deploybe.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/api")
 public class TestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping
-    public String hello() {
+    public String hello(HttpServletRequest request) {
+        logger.info("HELLO Request URL: {}", request.getRequestURL());
         return "Hello World";
     }
 
     @GetMapping("/test")
-    public String test() {
+    public String test(HttpServletRequest request) {
+        logger.info("TEST Request URL: {}", request.getRequestURL());
         return "Test Success!!!!";
     }
 }
